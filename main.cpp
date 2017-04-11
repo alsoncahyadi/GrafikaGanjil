9c7bda54710ca5cdb47d9d04c2cc489db7b0cd98
alsoncahyadi <alson.cahyadi@gmail.com>
2017-04-11 19:14:38 +0700

    finnish dude

--- main.cpp
+++ main.cpp
@@ -27,7 +27,9 @@ const float r = 1.0;
 const float g = 1.0;
 const float b = 1.0;
 const float DEG2RAD = 3.14159/180;
-const float scaleconst  = 0.5;
+const float scaleconst  = 0.002;
+const float dx = -400;
+const float dy = 400;
 
 void init() {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -81,7 +83,7 @@ void DrawCircle(float cx, float cy, float r, int num_segments, int begindraw, in
         float x = r * cosf(theta);//calculate the x component
         float y = r * sinf(theta);//calculate the y component
 
-        glVertex2f(x + cx, y + cy);//output vertex
+        glVertex2f(x + cx + dx, y + cy + dy);//output vertex
         glColor3f(r, g, b);
     }
     glEnd();
@@ -90,10 +92,62 @@ void DrawCircle(float cx, float cy, float r, int num_segments, int begindraw, in
 
 void DrawLine(float x1, float y1, float x2, float y2) {
     glBegin(GL_LINE_STRIP);
-    glVertex2f(x1, y1);
-    glVertex2f(x2, y2);
+    glVertex2f(x1 + dx, y1 + dy);
+    glVertex2f(x2 + dx, y2 + dy);
     glColor3f(r, g, b);
     glEnd();
+}
+
+void DrawDragon() {
+    // Sayap kiri depan
+    DrawLine(242, -120, 163, -79);
+    DrawLine(163, -79, 110, -111);
+    DrawLine(110,-111, 17, -193);
+    DrawLine(17, -193, 8, -216);
+
+    // Sayap kanan depan
+    DrawLine(371, -122, 445, -77);
+    DrawLine(445, -77, 511, -116);
+    DrawLine(511, -116, 593, -189);
+    DrawLine(593, -189, 608, -215);
+
+    // Sayap kiri belakang
+    DrawCircle(79, -265, 86.5, 1000, 140, 400);
+    DrawCircle(151, -232, 41, 1000, -80, 355);
+    DrawCircle(224, -275, 46, 1000, -50, 400);
+
+    // Sayap kanan belakang
+    DrawCircle(405, -293, 109/2, 1000, 170, 505);
+    DrawCircle(477, -244, 99/2, 1000, 225, 500);
+    DrawCircle(539, -255, 161/2, 1000, 120, 380);
+
+    // Selangkangan
+    DrawCircle(308, -320, 104/2, 1000, 100, 190); // selangkangan kiri
+    DrawCircle(308, -320, 104/2, 1000, 310, 390); // selangkangan kanan
+
+    // Ekor
+    DrawLine(287, -273, 301, -501);
+    DrawLine(301, -501, 286, -501);
+    DrawLine(286, -501, 304, -545);
+    DrawLine(304, -545, 326, -501);
+    DrawLine(326, -501, 309, -501);
+    DrawLine(309, -501, 330, -273);
+
+    // Pundak
+    DrawCircle(308, -35, 218/2, 1000, 630, 715);
+    DrawCircle(308, -35, 218/2, 1000, 783, 855);
+
+    // Leher
+    DrawLine(283, -139, 294, -98);
+    DrawLine(294, -98, 296, -58);
+    DrawLine(294, -98, 296, -58);
+    DrawLine(296, -58, 285, -76);
+    DrawLine(285, -76, 302, -9);
+    DrawLine(302, -9, 314, -9);
+    DrawLine(314, -9, 329, -71);
+    DrawLine(329, -71, 321, -56);
+    DrawLine(321, -56, 320, -106);
+    DrawLine(320, -106, 331, -145);
 }
 
 /* =========================================*/
@@ -108,8 +162,7 @@ void render(void) {
     glScalef(scaleconst, scaleconst, scaleconst);
 
     //=> SHAPES
-    DrawCircle(0.0, 0.0, 0.2, 100, 0, 110);
-    DrawLine(0.0, 1.0, 1.0, 0.0);
+    DrawDragon();
 
     Color bordercolor = {r,g,b};
     Color fillcolor = {r,g,b};
@@ -143,3 +196,9 @@ void mouse(int button, int state, int x, int y) {
         exit(0);
     }
 }
+
+
+
+
+
+
